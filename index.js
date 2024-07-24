e4891b7251109260a9c5fde7314d057e4167febf
Mohd Adnan <adnanakbar716@gmail.com>
2024-07-24 16:43:29 +0530

    revert the code

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let name = "Adnan";
 function myName() {
   console.log("my name", name);
 }
+<<<<<<< HEAD
 function second() {
   console.log("This is the second function");
 }
+=======
+myName();
+>>>>>>> parent of 114a5ac (remove the function call)
